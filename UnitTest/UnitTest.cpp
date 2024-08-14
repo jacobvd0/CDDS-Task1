199d40be095a8dbd1f0a44976cdca62b72d2205f
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-08-14 15:44:20 +0930

    Dynamic array tests

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -3,6 +3,7 @@
 #include "HashTable.h"
 #include "Critter.h"
 #include "raylib.h"
+#include "DynamicArray.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -34,17 +35,35 @@ namespace UnitTest
 		}
 	};
 
-	TEST_CLASS(OriginPointTests)
+	TEST_CLASS(DynamicArrayTests)
 	{
 	public:
 
 
-		TEST_METHOD(OriginTest)
+		TEST_METHOD(Adding)
 		{
-			Critter testCrit;
-			testCrit.SetOrigin({ 0.5,0.5 });
+			HashTable hashTable;
+			DynamicArray critterArray;
+			critterArray.Init(3, "TEST", &hashTable);
 			
-			Assert::AreEqual(13.5f, testCrit.OriginTest(27, 25).x);
+			
+			
+			Assert::AreEqual(true, critterArray[0]->critTest());
+		}
+
+		TEST_METHOD(Removing)
+		{
+			HashTable hashTable;
+			DynamicArray critterArray;
+			for (int i = 0; i <= 3; i++) { // Adds 3 critters to the array
+				critterArray.Init(3, "TEST", &hashTable);
+			}
+			
+			// Removes 1, so now there's 2
+			critterArray.Destroy(1);
+
+			// Checks if there's 2 critters
+			Assert::AreEqual(2, critterArray.GetCritCount());
 		}
 	};
 }
